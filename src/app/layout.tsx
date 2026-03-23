8bce87ebfafdf58d165749325d3960e91a7e1049
SKEIP96 <kadynjenya@gmail.com>
2026-03-23 13:50:30 +0200

    Navbar ready

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,18 @@
 import type { Metadata } from "next"
 import "../styles/globals.css"
 import Navbar from "@/components/layout/Navbar"
+import { Manrope } from "next/font/google";
 
 export const metadata: Metadata = {
   title: "Urbanouse",
   description: "Real estate website",
 }
+
+const manrope = Manrope({
+  subsets: ["latin"],
+  variable: "--font-manrope",
+  weight: ["400", "500", "600", "700", "800"],
+});
 
 export default function RootLayout({
   children,
@@ -15,7 +22,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <Navbar />
-      <body className="antialiased">
+      <body className={`${manrope.variable} antialiased`}>
         {children}
       </body>
     </html>
--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,10 +1,12 @@
 import Link from"next/link";
+import Image from "next/image";
 import {Button} from "@/components/ui/button";
+import { ChevronDown } from "lucide-react"
 
 const navLinks = [
     {label: "Home", href: "#"},
     {label: "About Us", href: "#"},
-    {label: "Properties", href: "#"},
+    {label: "Properties", href: "#", hasDropdown: true},
     {label: "Agents", href: "#"},
     {label: "Projects", href: "#"},
     {label: "Contact Us", href: "#"},
@@ -13,11 +15,17 @@ const navLinks = [
 export default function Navbar() {
     return (
       <header className="absolute top-0 left-0 w-full z-50">
-        <div className="container mx-auto px-20 pt-3">
+        <div className="container mx-auto px-10 pt-3">
             <div className="flex items-center justify-between">
                   {/* Logo */}
-                <Link href="/" className="font-semibold text-lg">
-                Urbanouse
+                <Link href="/" className="flex items-center font-semibold text-lg gap-1">
+                <Image 
+                src="\logos\logo-mark.svg"
+                alt="Urbanhouse logo"
+                width={20}
+                height={21}
+                />
+               <span className="font-bold text-xl">Urbanouse</span> 
                 </Link>
                 {/* Menu*/}
                 <nav className="flex items-center gap-5 text-sm text-zinc-600">
@@ -25,19 +33,22 @@ export default function Navbar() {
                         <Link
                         key={item.href}
                         href={item.href}
-                        className="hover:text-black transition"
+                        className="hover:text-black transition flex items-center gap-1"
                         >
-                        {item.label}
+                        <span>{item.label}</span>
+                        {item.hasDropdown && (
+                            <ChevronDown height={16} width={16} strokeWidth="1"/>
+                        )}
                         </Link>
                     ))}
                 </nav>
                    {/* Buttons */}
                    <div className="flex items-center gap-3">
-                    <Button variant="outline" className="h-10 rounded-full px-5">
-                        Sign In
-                    </Button>
-                    <Button  className="rounded-full h-10 px-5">
+                    <Button variant="outline" className="h-9 rounded-full px-5 border-2 border-black">
                         Sign Up
+                    </Button>
+                    <Button  className="rounded-full h-9 px-5">
+                        Sign In
                     </Button>
                 </div>
             </div>
