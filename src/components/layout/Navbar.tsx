02789141244f68b3558adf1b581d38ad5ac124f7
SKEIP96 <kadynjenya@gmail.com>
2026-03-26 11:35:54 +0200

    hero

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,59 +1,62 @@
-import Link from"next/link";
+import Link from "next/link";
 import Image from "next/image";
-import {Button} from "@/components/ui/button";
-import { ChevronDown } from "lucide-react"
+import { Button } from "@/components/ui/button";
+import { ChevronDown } from "lucide-react";
 
 const navLinks = [
-    {label: "Home", href: "#"},
-    {label: "About Us", href: "#"},
-    {label: "Properties", href: "#", hasDropdown: true},
-    {label: "Agents", href: "#"},
-    {label: "Projects", href: "#"},
-    {label: "Contact Us", href: "#"},
-    ];
+  { label: "Home", href: "#" },
+  { label: "About Us", href: "#" },
+  { label: "Properties", href: "#", hasDropdown: true },
+  { label: "Agents", href: "#" },
+  { label: "Projects", href: "#" },
+  { label: "Contact Us", href: "#" },
+];
 
 export default function Navbar() {
-    return (
-      <header className="absolute top-0 left-0 w-full z-50">
-        <div className="container mx-auto px-10 pt-3">
-            <div className="flex items-center justify-between">
-                  {/* Logo */}
-                <Link href="/" className="flex items-center font-semibold text-lg gap-1">
-                <Image 
-                src="\logos\logo-mark.svg"
-                alt="Urbanhouse logo"
-                width={20}
-                height={21}
-                />
-               <span className="font-bold text-xl">Urbanouse</span> 
-                </Link>
-                {/* Menu*/}
-                <nav className="flex items-center gap-5 text-sm text-zinc-600">
-                    {navLinks.map((item) =>(
-                        <Link
-                        key={item.href}
-                        href={item.href}
-                        className="hover:text-black transition flex items-center gap-1"
-                        >
-                        <span>{item.label}</span>
-                        {item.hasDropdown && (
-                            <ChevronDown height={16} width={16} strokeWidth="1"/>
-                        )}
-                        </Link>
-                    ))}
-                </nav>
-                   {/* Buttons */}
-                   <div className="flex items-center gap-3">
-                    <Button variant="outline" className="h-9 rounded-full px-5 border-2 border-black">
-                        Sign Up
-                    </Button>
-                    <Button  className="rounded-full h-9 px-5">
-                        Sign In
-                    </Button>
-                </div>
-            </div>
+  return (
+    <header className="absolute top-0 left-0 w-full z-50">
+      <div className="container mx-auto px-10 pt-3">
+        <div className="flex items-center justify-between">
+          {/* Logo */}
+          <Link
+            href="/"
+            className="flex items-center font-semibold text-lg gap-1"
+          >
+            <Image
+              src="images\logos\logo-mark.svg"
+              alt="Urbanouse logo"
+              width={20}
+              height={21}
+            />
+            <span className="font-bold text-xl">Urbanouse</span>
+          </Link>
+          {/* Menu*/}
+          <nav className="flex items-center gap-5 text-sm text-zinc-600">
+            {navLinks.map((item) => (
+              <Link
+                key={item.href}
+                href={item.href}
+                className="hover:text-black transition flex items-center gap-1"
+              >
+                <span>{item.label}</span>
+                {item.hasDropdown && (
+                  <ChevronDown height={16} width={16} strokeWidth="1" />
+                )}
+              </Link>
+            ))}
+          </nav>
+          {/* Buttons */}
+          <div className="flex items-center gap-3">
+            <Button
+              variant="outline"
+              className="h-9 rounded-full px-5 border-2 border-black bg-transparent"
+            >
+              Sign Up
+            </Button>
+            <Button className="rounded-full h-9 px-5">Sign In</Button>
+          </div>
         </div>
-    
-      </header>
-    )
-  }
+      </div>
+    </header>
+  );
+}
--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -1,5 +1,46 @@
+import Image from "next/image";
 export default function Hero() {
-    return (
-        
-    )
+  return (
+    <section className="w-full">
+      {/* Hero Wrapper */}
+      <div className="relative h-screen w-full overflow-hidden bg-[#EEF3F8]">
+        {/* Back Title */}
+        <div className="absolute top-40 left-1/2 z-20 w-[95vw] h-[50vw]  -translate-x-1/2">
+          <Image
+            src="/images/hero/Urbanouse.png"
+            alt="Urbanouse"
+            width={2000}
+            height={1200}
+            className="h-auto w-full object-contain "
+            priority
+          />
+        </div>
+        {/* House image */}
+        <div className="absolute top-60 left-1/2 z-20 w-[100vw] h-[50vw]  -translate-x-1/2">
+          <Image
+            src="/images/hero/3d-house-model.svg"
+            alt="Modern house"
+            width={2000}
+            height={1200}
+            className="h-auto w-full object-contain"
+            priority
+          />
+        </div>
+        {/* Bottom Conteiner */}
+        <div className="absolute bottom-10 left-6 right-6 z-30   flex items-end justify-between">
+          {/* Left text */}
+          <p className="max-w-[360px] text-white">
+            Discover meticulously crafted homes and properties, blending
+            contemporary aesthetics with sustainable living.
+          </p>
+          {/* Right blick */}
+          <div className="max-w-[360px] text-white">
+            <p className="text-lg font-medium">Silverstone Residence</p>
+            <p className="mt-1 text-sm text-white/80">1234 Sunflower Lane</p>
+            <p className="text-sm text-white/80">Los Angeles, CA 90001</p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
